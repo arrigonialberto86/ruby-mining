149d414318ee57abc2a7412ee13dd8102a4a1f9b
arrigonialberto86 <arrigonialberto86@gmail.com>
2013-05-31 16:07:54 +0200

    first commit

---
+++ Jarfile
@@ -0,0 +1,3 @@
+jar 'nz.ac.waikato.cms.weka:weka-stable','3.6.6'
+jar 'org.apache.commons:commons-math','2.0'
+jar 'junit:junit','3.8.1'
---
+++ lib/ruby-mining.rb
@@ -0,0 +1,17 @@
+require "jbundler"
+require "java"
+java_import "weka.clusterers.SimpleKMeans"
+java_import "org.apache.commons.math.random.RandomDataImpl"
+
+
+module Apache_test
+  def Apache_test.print_test 
+    randomData = RandomDataImpl.new 
+    10.times do
+      puts randomData.nextLong(1, 1000000)
+    end
+  end
+end
+
+
+
