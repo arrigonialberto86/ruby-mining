4dded449ff8f8bb8b3ec07e103acfaf65135bd5f
arrigonialberto86 <arrigonialberto86@gmail.com>
2013-06-12 17:55:57 +0200

    added methods to build in-memory Instances dataset

--- lib/ruby_mining/core/type/instances.rb
+++ lib/ruby_mining/core/type/instances.rb
@@ -9,8 +9,17 @@ module Core
     java_import 'weka.core.converters.CSVSaver'
     java_import 'weka.core.converters.ArffSaver'
     java_import "weka.core.FastVector"
+    java_import "weka.core.Instance"
 
     class Instances
+
+      def ReturnAttrData(att)
+        attr_values = []
+            enumerateInstances.each do |i|
+          attr_values << i.value(attribute(att))
+        end
+        attr_values
+      end
       
       def mean(att) 
         sum = enumerateInstances.inject(0) do |s,x|
@@ -34,7 +43,46 @@ module Core
         saver.setFile(out_file);
         saver.writeBatch();
       end
-    end
+
+      # should check that the array is bidimensional and the lengths are equal
+      def check_array(data)
+        return true
+      end
+
+      # data is inserted 'by row', i.e. one Instance object is inserted at the time
+      def populate_by_row(data)
+        unless check_array(data) == false
+          data.each do |row|
+            insert_instance(row)
+          end
+        end
+      end
+
+      def insert_instance(instance)
+        data_ref=Array.new
+        instance.each_with_index do |attribute,idx|
+          data_ref << insert_attribute(attribute,idx)
+        end
+        double_array = data_ref.to_java :double
+        single_row = Instance.new(1.0, double_array)
+        self.add(single_row)
+      end
+
+      def insert_attribute(attribute_value,position)
+        att=attribute_value
+        if self.attribute(position).isNumeric
+          return attribute_value
+        elsif self.attribute(position).isNominal
+          idx = self.attribute(position).indexOfValue(attribute_value)
+          return idx
+        elsif self.attribute(position).isDate
+          date = self.attribute(position).ParseDate(attribute_value)
+          return date
+        else
+          puts 'Attribute type is unknown!'
+        end
+      end
+    end #Instances class
 
     def Type.create_instances(name,attributes)
       attributes_vector = FastVector.new
